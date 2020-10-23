306231af819afc42645448216682413fc2c536a2
Pierre Guillen <hi@cerbero.dev>
2020-10-23 16:54:28 -0500

    Modularized Widgets

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutternews/views/widgets/category_burble.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
@@ -38,55 +39,6 @@ class HomeView extends StatelessWidget {
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class CategoryBurble extends StatelessWidget {
-  const CategoryBurble({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('Tap to button category');
-      },
-      child: Container(
-        height: 150,
-        width: 120,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.asset(
-                  'assets/123.jpg',
-                  fit: BoxFit.cover,
-                  width: 60,
-                  height: 60,
-                ),
-              ),
-            ),
-            SizedBox(height: 5),
-            Container(
-              alignment: Alignment.center,
-              height: 30,
-              width: 60,
-              child: Text(
-                'Politica',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            )
-          ],
         ),
       ),
     );
